99614b060367e14a94c4055db866809e1066c3d4
Fernando Calmet <fercalmet@gmail.com>
2021-12-28 21:00:47 -0500

    added main menu

--- 1.Basics/Program.cs
+++ 1.Basics/Program.cs
@@ -1,16 +1,48 @@
-﻿namespace _1.Basics
+﻿using System;
+
+namespace _1.Basics
 {
-    internal class Program
+    public class Program
     {
+        static int option = 0;
+
         static void Main(string[] args)
         {
             Lesson05.Examples lesson05 = new Lesson05.Examples();
-            lesson05.SumGenerator();
-            lesson05.FullNameGenerator();
-
             Lesson06.Examples lesson06 = new Lesson06.Examples();
-            lesson06.Example1();
-            lesson06.Example2();
+
+            do
+            {
+                Console.WriteLine("Select a number option: \n");
+                Console.WriteLine("0: Lesson05 - SumGenerator");
+                Console.WriteLine("1: Lesson05 - FullNameGenerator");
+                Console.WriteLine("2: Lesson06 - Example 1");
+                Console.WriteLine("3: Lesson06 - Example 2");
+
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 0:
+                        lesson05.SumGenerator();
+                        break;
+                    case 1:
+                        lesson05.FullNameGenerator();
+                        break;
+                    case 2:
+                        lesson06.Example1();
+                        break;
+                    case 3:
+                        lesson06.Example2();
+                        break;
+                    default:
+                        Environment.Exit(0);
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+            while (option < 4);
         }
     }
 }
