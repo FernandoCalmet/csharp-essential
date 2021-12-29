bd6620fc0b83e8572ad02b28fefb44ad2d46a4d3
Fernando Calmet <fercalmet@gmail.com>
2021-12-28 20:27:59 -0500

    minor changes

---
+++ 1.Basics/Lesson05/Example2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1.Basics.Lesson05.FullNameGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your last name:");
+            string lastName = Console.ReadLine();
+
+            string fullName = name + " " + lastName;
+            Console.WriteLine($"Your full name is: {fullName}");
+
+            Console.ReadKey();
+        }
+    }
+}
